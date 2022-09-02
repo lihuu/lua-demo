491c42a2ab1c12a4c9a116ae39bd55b96a83e036
lihu <1449488533qq@gmail.com>
2022-09-02 12:52:40 +0800

    add lua next demo

--- ListDemo.lua
+++ ListDemo.lua
@@ -13,3 +13,17 @@ array[#array + 1] = "World"
 for _, value in pairs(array) do
   print(value)
 end
+
+local index, value = next(programLanguages)
+print('index: ' .. index .. ", value: " .. value)
+
+index, value = next(programLanguages, 1)
+print('index: ' .. index .. ", value: " .. value)
+
+index, value = next(programLanguages, 2)
+if index ~= nil or value ~= nil then
+  print('index: ' .. index .. ", value: " .. value)
+else
+  print("index is nil or value is nil")
+end
+
