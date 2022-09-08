6a986b91db8a659652d1dea3d341b3c718937a88
lihu <1449488533qq@gmail.com>
2022-09-08 17:11:38 +0800

    redis lock amount

---
+++ redis-lock-ticket.lua
@@ -0,0 +1,13 @@
+local exist = redis.call('exist', KEYS[1]);
+if exist == 1 then
+  local limitCount = redis.call('get', KEYS[2]);
+  if limitCount < KEYS[3] then
+    return 0;
+  end
+  redis.call('incrby', KEYS[1], KEYS[3]);
+  redis.call('decrby', KEYS[2], KEYS[3])
+  return 1
+else
+  return -1;
+end
+
