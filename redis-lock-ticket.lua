1b025eb410349645f430a8d6379eaa24b54ceb73
lihu <1449488533qq@gmail.com>
2022-09-20 17:37:35 +0800

    fix

--- redis-lock-ticket.lua
+++ redis-lock-ticket.lua
@@ -1,11 +1,11 @@
 local exist = redis.call('exists', KEYS[1])
 if exist == 1 then
-  local limitCount = redis.call('get', KEYS[2])
-  if limitCount < KEYS[3] then
+  local limitCount = tonumber(redis.call('get', KEYS[2]))
+  if limitCount < tonumber(ARGV[1]) then
     return 0
   end
-  redis.call('incrby', KEYS[1], KEYS[3])
-  redis.call('decrby', KEYS[2], KEYS[3])
+  redis.call('incrby', KEYS[1], ARGV[1])
+  redis.call('decrby', KEYS[2], ARGV[1])
   return 1
 else
   return -1;
