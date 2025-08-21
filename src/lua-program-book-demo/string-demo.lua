d06c731b63b70757dcae041affa21bb85ac2406e
SilentStorm <1449488533qq@gmail.com>
2025-08-22 00:28:18 +0800

    refactor: comment out unit test code and retain object methods

--- src/lua-program-book-demo/string-demo.lua
+++ src/lua-program-book-demo/string-demo.lua
@@ -1,7 +1,22 @@
-local luaunit = require('luaunit')
-local str = "one thing"
-local result = string.gsub(str, "one", "another")
-luaunit.assertEquals("another thing", result)
-print(result)
+-- local luaunit = require('luaunit')
+-- local str = "one thing"
+-- local result = string.gsub(str, "one", "another")
+-- luaunit.assertEquals("another thing", result)
+-- print(result)
 
+local obj = {}
 
+function obj.hello(self)
+    print(self)
+    return "Hello, World!"
+end
+
+function obj:test()
+    print(self)
+    return "Hello, Test!"
+end
+
+print(obj.hello())
+
+print(obj:hello())
+print(obj:test())
