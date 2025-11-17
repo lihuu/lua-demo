5d22f5a5c07fa7c57d140b6c3a5102baf32035e7
lihuu <lihu1449488533@outlook.com>
2025-11-17 12:24:55 +0800

    feat: add IOTest script to read and print lines from utils.lua

---
+++ src/IOTest.lua
@@ -0,0 +1,7 @@
+io.input("utils.lua")
+
+for line in io.lines() do
+	print(line)
+end
+
+io.close()
--- src/lua-program-book-demo/string-demo.lua
+++ src/lua-program-book-demo/string-demo.lua
@@ -20,3 +20,22 @@ print(obj.hello())
 
 print(obj:hello())
 print(obj:test())
+
+local function startWith(str, prefix)
+    return string.find(str, "^" .. prefix) == 1
+end
+
+local function checkStartWith(str,prefix)
+    if startWith(str, prefix) then
+        print("String '" .. str .. "' starts with prefix '" .. prefix .. "'")
+    else
+        print("String '" .. str .. "' does not start with prefix '" .. prefix .. "'")
+    end
+    
+end
+
+checkStartWith("hello world", "hello")
+checkStartWith("hello world", "world")
+checkStartWith("----lua programming", "----")
+
+
